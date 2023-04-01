60a87074dd9bb8480cbf341616a7407d7da4d74e
yosi90 <yosi121990@hotmail.es>
2023-04-02 00:16:58 +0100

    firtebase instalado, directorio cambiado, nombre proyecto cambiado

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# PrimeraApp
+# fichas3.5
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.0.0.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'primeraApp'`, () => {
+  it(`should have as title 'fichas3.5'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('primeraApp');
+    expect(app.title).toEqual('fichas3.5');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('primeraApp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('fichas3.5 app is running!');
   });
 });
